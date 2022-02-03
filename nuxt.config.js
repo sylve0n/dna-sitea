d7c7c6f442dcafb05993d69139f70e9ccf3bcc40
Hannah S <hannahsartin@gmail.com>
2022-02-02 19:24:20 -0600

    Fixing circle image

--- nuxt.config.js
+++ nuxt.config.js
@@ -111,8 +111,8 @@ export default {
   },
   pwa: {
     icon: {
-      source: 'static/icon.png',
-      filename: 'icon.png'
+      source: 'static/doling-circle.png',
+      filename: 'doling-circle.png'
     },
     manifest: { name: SITE_INFO.sitename || process.env.npm_package_name || '', lang: process.env.lang },
     meta: {
